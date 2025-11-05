daace8be9c9c3abc1f01e784413c0df792435ce1
Maurice Guikema <maurice@mgui.nl>
2025-11-05 14:13:02 +0100

    remove unused tasks

--- app/apps/aliassen/tasks.py
+++ app/apps/aliassen/tasks.py
@@ -29,19 +29,6 @@ class BaseTaskWithRetry(celery.Task):
 
 
 @shared_task(bind=True, base=BaseTaskWithRetry)
-def task_maak_bijlagealias(self, bijlage_url, taakgebeurtenis_id):
-    from apps.aliassen.models import BijlageAlias
-    from apps.taken.models import Taakgebeurtenis
-
-    bijlage_instance = BijlageAlias.objects.create(
-        bron_url=bijlage_url,
-        taak_gebeurtenis=Taakgebeurtenis.objects.get(pk=taakgebeurtenis_id),
-    )
-
-    return f"BijlageAlias id: {bijlage_instance.pk}"
-
-
-@shared_task(bind=True, base=BaseTaskWithRetry)
 def task_update_melding_alias_data_for_all_meldingen(self, cache_timeout=0):
     from apps.aliassen.models import MeldingAlias
 
@@ -59,18 +46,6 @@ def task_update_melding_alias_data_for_all_meldingen(self, cache_timeout=0):
         melding_alias.update_zoek_data()
 
     return f"updated/totaal={melding_alias_items_for_update.count()}/{all_melding_alias_items.count()}"
-
-
-def _update_melding_alias_data(melding_alias_id):
-    from apps.aliassen.models import MeldingAlias
-
-    melding_alias = MeldingAlias.objects.filter(pk=melding_alias_id).first()
-    if melding_alias:
-        melding_alias.valideer_bron_url()
-        melding_alias.save()
-        melding_alias.update_zoek_data()
-
-    return f"MeldingAlias with id={melding_alias_id}, updated"
 
 
 @shared_task(bind=True, base=BaseTaskWithRetryBackoff)
